5e38619ad0f3bf2e8376b5a785a4002ff9a404a2
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-04 21:13:52 -0700

    flexbox tweaking

--- src/components/CommentList/CommentForm.jsx
+++ src/components/CommentList/CommentForm.jsx
@@ -1,25 +1,36 @@
 import defaultIcon from '../../assets/images/Mohan-muruge.jpg';
 import './CommentForm.scss';
+import commentIcon from '../../assets/icons/add_comment.svg';
 
 function CommentForm(props) {
     return (
         <>
-            <section className="comment-form">
+            <article className="comment-form dividing-line">
                 <h3>{props.commentsLength} Comments</h3>
-                <img src={defaultIcon} className="comment-form__icon" />
-                <p>JOIN THE CONVERSATION</p>
-                <form className="comment-form__container">
-                    <div>
-                        <input
-                            className="comment-form__input"
-                            name="textbox"
-                        ></input>
-                        <button className="comment-form__button">
-                            ADD A COMMENT
-                        </button>
-                    </div>
-                </form>
-            </section>
+                <div className="comment-form__icon-form-pair">
+                    <img src={defaultIcon} className="comment-form__icon" />
+                    <section className="comment-form__container--outer">
+                        <p>JOIN THE CONVERSATION</p>
+                        <form className="comment-form__container--inner">
+                            <input
+                                className="comment-form__input"
+                                name="textbox"
+                            ></input>
+
+                            <div className="submit-button__container">
+                                <button className="submit-button__button">
+                                    ADD A COMMENT
+                                </button>
+                                <img
+                                    src={commentIcon}
+                                    alt="comment"
+                                    className="submit-button__icon"
+                                />
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </article>
         </>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,50 @@
 import './Header.scss';
 import search from '../assets/icons/search.svg';
 import logo from '../assets/images/BrainFlix-logo.svg';
+import defaultImg from '../assets/images/Mohan-muruge.jpg';
+import uploadImg from '../assets/icons/upload.svg';
 
 function Header() {
     return (
         <>
             <section className="header__container">
-                <img src={logo} />
-                <div id="input__container">
-                    <input type="text" id="input__textbox" />
-                    <img src={search} id="input__img" />
+                <img src={logo} alt="BrainFlix" />
+                <div className="header__navbar">
+                    <div className="header__search-icon-pair">
+                        <div className="header__search--container">
+                            <input
+                                type="text"
+                                className="header__search--textbox"
+                            />
+                            <img
+                                src={search}
+                                className="header__search--icon "
+                                alt="Search"
+                            />
+                        </div>
+                        <img
+                            src={defaultImg}
+                            className="header__default-img header__default-img--mobile"
+                            alt=""
+                        />
+                    </div>
+
+                    <div className="header__upload--container">
+                        <button className="header__upload--button">
+                            UPLOAD
+                        </button>
+                        <img
+                            src={uploadImg}
+                            className="header__upload--icon"
+                            alt=""
+                        />
+                    </div>
+
+                    <img
+                        src={defaultImg}
+                        className="header__default-img header__default-img--tablet"
+                        alt=""
+                    />
                 </div>
             </section>
         </>
