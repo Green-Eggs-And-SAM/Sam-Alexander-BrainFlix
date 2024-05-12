68f8e5175ee41e7d2c785823f36c48f23cac761d
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-11 17:27:53 -0700

    Upload styled

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Upload.scss';
+import defaulThumbnail from '../assets/images/Upload-video-preview.jpg';
+import publishImg from '../assets/icons/publish.svg';
 
 function Upload() {
     const formRef = useRef(null);
@@ -18,51 +20,83 @@ function Upload() {
         console.log(title);
         console.log(description);
 
+        let returnTrigger = false;
         if (!title) {
-            alert('You must fill out the title');
             setTitleValid(false);
-            return;
+            returnTrigger = true;
         } else {
             setTitleValid(true);
         }
 
         if (!description) {
-            alert('You must fill out the desciption');
             setDescriptionValid(false);
-            return;
+            returnTrigger = true;
         } else {
             setDescriptionValid(true);
         }
-        alert('Your video is uploading');
-        navigate('/');
+
+        if (returnTrigger) {
+            alert('Title and Description must be filled out.');
+            return;
+        } else {
+            alert('Your video is uploading');
+            navigate('/');
+        }
     };
 
     return (
-        <>
+        <article className="upload">
             <h1>UPLOAD PAGE</h1>
-            <form ref={formRef}>
-                <label>
-                    TITLE YOUR VIDEO:{' '}
-                    <input
-                        type="text"
-                        name="title"
-                        className={`${titleValid ? '' : 'upload__empty'}`}
-                    />
-                </label>
-                <label>
-                    ADD A VIDEO DESCRIPTION:{' '}
-                    <input
-                        type="text"
-                        name="description"
-                        className={`${descriptionValid ? '' : 'upload__empty'}`}
-                    />
-                </label>
-
-                <button type="button" onClick={handleSubmit}>
-                    PUBLISH
-                </button>
-            </form>
-        </>
+            <section>
+                <div className="upload__main-container">
+                    <div className="upload__thumbnail-container">
+                        <h5 className="upload__subheader">VIDEO THUMBNAIL</h5>
+                        <img
+                            className="upload__thumbnail"
+                            src={defaulThumbnail}
+                        ></img>
+                    </div>
+                    <form ref={formRef} className="upload__form">
+                        <label className="upload__subheader">
+                            TITLE YOUR VIDEO:{' '}
+                            <input
+                                type="text"
+                                name="title"
+                                className={`${
+                                    titleValid ? '' : 'upload__empty'
+                                }`}
+                            />
+                        </label>
+                        <label className="upload__subheader">
+                            ADD A VIDEO DESCRIPTION:{' '}
+                            <input
+                                type="text"
+                                name="description"
+                                className={`${
+                                    descriptionValid ? '' : 'upload__empty'
+                                }`}
+                            />
+                        </label>
+                    </form>
+                </div>
+                <div className="divider hide-divider"></div>
+                <div className="upload__button-pair-conatiner">
+                    <div className="button__container button__width">
+                        <button type="button" onClick={handleSubmit}>
+                            PUBLISH
+                        </button>
+                        <img
+                            src={publishImg}
+                            className="header__upload--icon"
+                            alt=""
+                        />
+                    </div>
+                    <Link to={`/`} className="button__width">
+                        <button className="button__inverted">CANCEL</button>
+                    </Link>
+                </div>
+            </section>
+        </article>
     );
 }
 
